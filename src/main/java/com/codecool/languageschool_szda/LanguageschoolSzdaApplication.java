a43e25406c2f072dc037045b0b40917eb22b62f1
zekely David Attila <szekelydavidattila@gmail.com>
2021-08-08 21:13:21 +0200

    initial

---
+++ src/main/java/com/codecool/languageschool_szda/LanguageschoolSzdaApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.languageschool_szda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageschoolSzdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageschoolSzdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/languageschool_szda/LanguageschoolSzdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.languageschool_szda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageschoolSzdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
